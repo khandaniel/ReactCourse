ab631ba60df038f0298a6933234df6a3f09e3fe2
Khan Daniel <daniel.khan@catenamedia.com>
2020-08-09 22:21:12 +0300

    functional refactoring of GoodsList component

--- src/GoodsList/GoodsList.js
+++ src/GoodsList/GoodsList.js
@@ -1,37 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 import GoodsListElement from '../GoodsListElement/GoodsListElement';
 import PropTypes from 'prop-types';
 
-export default class GoodsList extends Component {
-  constructor(props) {
-    super(props);
-    this.onDelete = (id) => {
-      this.props.onDelete(id);
-    };
-  }
-
-  render() {
-    const { goods, categories, selectedItems, onElementToggle } = this.props;
-    return (
-      <div>
-        {Array.isArray(goods) && goods.map( (item) => {
-          const selected = selectedItems.indexOf(item.id) >= 0;
-          return (
-            <GoodsListElement
-              item={ item }
-              categories={ categories }
-              key={ item.id }
-              selected={ selected }
-              onSave={ this.props.onElementUpdate }
-              onDelete={ this.onDelete }
-              onToggle={ onElementToggle }
-            />
-          );
-        })}
-      </div>
-    );
-  }
-}
+const GoodsList = (props) => {
+  const { goods, categories, selectedItems, onElementToggle, onDelete } = props;
+  return (
+    <div>
+      { goods.map((item) => {
+        const selected = selectedItems.indexOf(item.id) >= 0;
+        return (
+          <GoodsListElement
+            item={ item }
+            categories={ categories }
+            key={ item.id }
+            selected={ selected }
+            onSave={ props.onElementUpdate }
+            onDelete={ onDelete }
+            onToggle={ onElementToggle }
+          />
+        );
+      }) }
+    </div>
+  );
+};
 
 GoodsList.defaultProps = {
   goods: [],
@@ -46,3 +37,5 @@ GoodsList.propTypes = {
   onElementToggle: PropTypes.func,
   onElementUpdate: PropTypes.func,
 };
+
+export default GoodsList;
